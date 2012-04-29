3225e5b918cc220286d24d8afc0f302ba707e474
i.enable.much@gmail.com <i.enable.much@gmail.com@4bad88e1-9c75-ba03-59de-a41e83eabc90>
2012-04-29 02:12:45 +0000

    Initial code sync

---
+++ SoQna/SoQna/Models/_QuestionAndAnswer.cs
@@ -0,0 +1,16 @@
+﻿
+namespace SoQna.Models
+{
+    public class Question
+    {
+        public int QuestionId { get; set; }
+        public string QuestionText { get; set; }
+    }
+
+    public class Answer
+    {
+        public int AnswerId { get; set; }
+        public Question Question { get; set; }
+        public string AnswerText { get; set; }
+    }
+}
